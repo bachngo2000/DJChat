1e20e18ea961155a6e1d570838ff6cb71ade5c5b
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-08-28 15:12:35 -0500

    created an API endpoint for filtering servers by ServerID

--- DjangoChat/server/views.py
+++ DjangoChat/server/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
@@ -26,6 +27,13 @@ class ServerListViewSet(viewsets.ViewSet):
         # capture number of servers
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+
+        # we may want to pre check whether the user is actually logged in or not before we allow the user to access some of the different end points here
+        # So if the by user if the user ID is sent as a parameter to this endpoint and the user is not logged in, then we're going to tell them that the authentication is failed. You have to be authenticated to actually utilize that particular filter.
+        # Now we can extend that and say, well, if in actual fact, if the by_user or the by_server ID parameter is sent across in the request, then the user has to be logged in. If they're not, then they are going to be provided the authentication failed response.
+        if by_user or by_serverid and not request.user.is_authenticated:
+            raise AuthenticationFailed()
 
         # we're storing the category id/data if it exists
         if category:
@@ -39,6 +47,13 @@ class ServerListViewSet(viewsets.ViewSet):
         if qty:
             # items from the beginning through int(qty)-1
             self.queryset = self.queryset[: int(qty)]
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id {by_serverid} not found")
+            except ValueError:
+                raise ValidationError(detail=f"Server value error")
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
