fc1d610a921be088af8e359890f8b11f8d6b0cc0
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-08-28 12:45:45 -0500

    added an API endpoint for filtering servers by quantity

--- DjangoChat/server/views.py
+++ DjangoChat/server/views.py
@@ -23,7 +23,7 @@ class ServerListViewSet(viewsets.ViewSet):
     def list(self, request):
         # capture the category id that is being passed into this endpoint from the get request that was sent
         category = request.query_params.get("category")
-        # number of servers
+        # capture number of servers
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
 
@@ -37,7 +37,7 @@ class ServerListViewSet(viewsets.ViewSet):
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
         if qty:
-            # # items from the beginning through int(qty)-1
+            # items from the beginning through int(qty)-1
             self.queryset = self.queryset[: int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True)
