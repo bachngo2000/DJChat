d1c19002280a9175a4bfd117943dbc6824648c3f
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-01 15:26:02 -0500

    finished open and close functionalities for the primary app bar drawer

--- reactchat/src/pages/templates/PrimaryAppBar.tsx
+++ reactchat/src/pages/templates/PrimaryAppBar.tsx
@@ -1,18 +1,36 @@
-import { AppBar, Box, IconButton, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Drawer, IconButton, Link, Toolbar, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu"
+import { useState } from "react";
 
 
 const PrimaryAppBar = () => {
+    const [sideMenu, setSideMenu] = useState(false); 
     const theme = useTheme();
+
+    const toggleDrawer = (open: boolean) =>
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        (event: React.MouseEvent) => {
+            setSideMenu(open);
+        };
+
     return(
-        <AppBar sx = {{backgroundColor: theme.palette.background.default, borderBottom: `2px solid ${theme.palette.divider}`}}>
+        <AppBar sx = {{ zIndex:(theme) => theme.zIndex.drawer + 2, backgroundColor: theme.palette.background.default, borderBottom: `2px solid ${theme.palette.divider}`}}>
             <Toolbar variant="dense" sx={{height: theme.primaryAppBar.height, minHeight: theme.primaryAppBar.height}}>
                 <Box sx={{display: {xs: "block", sm: "none"}}}>
-                    <IconButton>
+                    <IconButton color="inherit" aria-label="open drawer" onClick={toggleDrawer(true)} edge="start" sx={{mr:2}}>
                         <MenuIcon />
                     </IconButton>
                 </Box>
+
+                <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+                    {[...Array(100)].map((_, i) => (
+                        <Typography key={i} paragraph>
+                            {i + 1}
+                        </Typography>
+                    ))}
+                </Drawer>
+                
                 <Link href="/" underline="none" color="inherit"> 
                     <Typography 
                         variant="h4" 
