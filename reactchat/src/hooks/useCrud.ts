0f07fa45507bb1147cd30fc35ebc4978520c260a
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-09-06 15:29:40 -0500

    built custom hook for CRUD operations

---
+++ reactchat/src/hooks/useCrud.ts
@@ -0,0 +1,52 @@
+import useAxiosWithInterceptor from "../helpers/jwtinterceptor";
+import { BASE_URL } from "../config"
+import { useEffect, useState } from 'react'
+
+// CREATE RETURN/READ UPDATE DELETE
+// declared an interaface that has generic type T
+// the interface defines 4 paremeters
+interface IuseCrud<T> {
+    dataCRUD: T[];
+    // function returning a promise without a value
+    fetchData: () => Promise<void>;
+    error: Error | null;
+    isLoading: boolean;
+}
+
+// create a React hook to provide all the centralized functionality to perform the CRUD operations on our backend API
+// hook is a special function that allows us to add state and other React features to functional components -- sort of like a class
+// a way of reusing stateful logic w/o the need for class components. Ex built-in hooks : useState(), useEffect(), etc.
+// this React hook/function has 2 parameters: initalData of type T[] and apiURL of type String
+const useCrud = <T>(initalData: T[], apiURL: string): IuseCrud<T> => {
+    const jwtAxios = useAxiosWithInterceptor();
+    // create a new state, initial state is empty
+    const [dataCRUD, setDataCRUD] = useState<T[]>(initalData)
+    // create a new state to store the error
+    const [error, setError] = useState<Error | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
+ 
+    // building a function to fetch data from the API
+    const fetchData = async () => {
+        setIsLoading(true)
+        try{
+            // build an Axios request to the API Server through the interceptor
+            const response = await jwtAxios.get(`${BASE_URL}${apiURL}`, {})
+            const data = response.data
+            //load up the data here
+            setDataCRUD(data)
+            setError(null)
+            setIsLoading(false)
+            return data;
+        } catch (error: any){
+            if (error.response && error.response.status === 400) {
+                setError(new Error("400"))
+            }
+            setIsLoading(false)
+            throw error;
+        }
+    };
+
+    // return back to the components so it can be utilized when a component utilizes our custom React hook
+    return {fetchData, dataCRUD, error, isLoading}
+}
+export default useCrud;
--- reactchat/src/pages/templates/SecondaryDraw.tsx
+++ reactchat/src/pages/templates/SecondaryDraw.tsx
@@ -2,21 +2,20 @@ import { Box, Typography } from "@mui/material";
 
 import {useTheme} from "@mui/material/styles";
 
-import axios from "axios";
+// import axios from "axios";
 
-import useAxiosWithInterceptor from "../../helpers/jwtinterceptor";
 
 const SecondaryDraw = () => {
 
     const theme = useTheme();
 
-    const jwtAxios = useAxiosWithInterceptor();
+    // const jwtAxios = useAxiosWithInterceptor();
 
-    jwtAxios.get("http://127.0.0.1:8000/api/server/select/?category=Internships").then(response => {
-        console.log(response.data);
-    }).catch((error) => {
-        console.log(error);
-    })
+    // jwtAxios.get("http://127.0.0.1:8000/api/server/select/?category=Internships").then(response => {
+    //     console.log(response.data);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
     
     return(
         <Box sx={{minWidth: `${theme.secondaryDraw.width}px`, height: `calc(100vh - ${theme.primaryAppBar.height}px)`, mt: `${theme.primaryAppBar.height}px`, borderRight: `1px solid ${theme.palette.divider}`, display: {xs: "none", sm:"block"}, overflow: "auto" }}>
